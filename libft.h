cd25c3d84a6b2be4ee013e3e97ad76b4a9686efb
Konrad Muhlbauer <kmuhlbau@2-D-7.42heilbronn.de>
2024-10-09 10:20:27 +0200

    Implemented strjoin

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 21:58:39 by kmuhlbau          #+#    #+#             */
-/*   Updated: 2024/10/08 11:09:28 by kmuhlbau         ###   ########.fr       */
+/*   Updated: 2024/10/09 10:17:14 by kmuhlbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
