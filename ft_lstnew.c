da267ac4e7ffed160337eb78069192994e5e7788
Konrad Muhlbauer <kmuhlbau@2-E-6.42heilbronn.de>
2024-10-10 17:13:17 +0200

    Implemented lstnew and added to srcs list for makefile and libft

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,11 +6,20 @@
 /*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 15:41:50 by kmuhlbau          #+#    #+#             */
-/*   Updated: 2024/10/10 15:42:28 by kmuhlbau         ###   ########.fr       */
+/*   Updated: 2024/10/10 17:03:26 by kmuhlbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	a(void)
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
 {
-	return (0);
+	t_list	*new_lst;
+
+	new_lst = malloc(sizeof(t_list));
+	if (!new_lst)
+		return (NULL);
+	new_lst->next = NULL;
+	new_lst->content = content;
+	return (new_lst);
 }
